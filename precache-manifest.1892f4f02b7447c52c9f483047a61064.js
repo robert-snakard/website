0c28d810c3b326bff98d7938ec537884f4325e87
robert-snakard <robert.snakard@gmail.com>
2018-12-19 14:17:11 -0800

    Updates

---
+++ precache-manifest.1892f4f02b7447c52c9f483047a61064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cfa78e2086b987ad603",
+    "url": "/website/static/css/main.ef9066a3.chunk.css"
+  },
+  {
+    "revision": "0cfa78e2086b987ad603",
+    "url": "/website/static/js/main.0cfa78e2.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/website/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f4e0dfd39ee5b717c3bfc889354e05b",
+    "url": "/website/index.html"
+  }
+];
