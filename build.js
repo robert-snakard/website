81768f0b892f07ed6994bbd15c0de8f079cd0c9c
Robert Snakard <robert.snakard@gmail.com>
2019-10-14 16:17:40 -0700

    render index with templating

--- build.js
+++ build.js
@@ -1,7 +1,29 @@
-var Metalsmith = require('metalsmith'),
-    markdown   = require('metalsmith-markdown');
+'use strict';
+
+const Metalsmith = require('metalsmith'),
+      layouts    = require('metalsmith-layouts'),
+      inplace    = require('metalsmith-in-place');
+
+const toUpper = function (string) {
+    "use strict";
+    return string.toUpperCase();
+}
+
+const templateConfig = {
+    engineOptions: {
+        filters: {
+            toUpper: toUpper
+        }
+    }
+}
 
 Metalsmith(__dirname)
-    .use(markdown())
+    .clean(true)
+    .source('./src')
     .destination('./build')
-    .build(function (err) { if(err) console.log(err) })
+    .use(layouts(templateConfig))
+    .use(inplace(templateConfig))
+    .build(function (err) { 
+        if(err) 
+            console.log(err) 
+    });
