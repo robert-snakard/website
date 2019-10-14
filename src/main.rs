142264b474ed715ccd1aca1e961a3073e7ad1e0f
Robert Snakard <robert.snakard@gmail.com>
2019-10-13 20:29:03 -0700

    cargo run now renders index.html

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use std::fs::File;
+use std::io::Write;
 use tera::{Tera, Context};
 
-fn main() -> Result<(), tera::Error>{
+fn main() -> Result<(), Box<dyn std::error::Error>>{
     let tera = Tera::new("assets/templates/**/*")?;
+    let ctx = Context::new();
+    let html = tera.render("index.html", &ctx)?;
 
-    let mut ctx = Context::new();
-    ctx.insert("title", "helloworld");
-
-    let html = tera.render("content.html", &ctx)?;
-    println!("{}", html);
+    let mut file = File::create("index.html")?;
+    file.write_all(&html.into_bytes())?;
     Ok(())
 }
